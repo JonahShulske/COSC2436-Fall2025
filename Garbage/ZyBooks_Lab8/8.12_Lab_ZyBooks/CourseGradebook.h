aab762d2c0dac25105a6495fc54208218ee3e9b6
Jonah Shulske <jonah.shulske@my.tccd.edu>
2025-10-22 10:28:30 -0500

    ZyBooks Sucks

--- Garbage/ZyBooks_Lab8/8.12_Lab_ZyBooks/CourseGradebook.h
+++ Garbage/ZyBooks_Lab8/8.12_Lab_ZyBooks/CourseGradebook.h
@@ -5,20 +5,14 @@
 #include <tuple>
 #include "Gradebook.h"
 
-struct tuple_hash
-{
-    template <class T1, class T2>
-    std::size_t operator() (const std::tuple<T1, T2>& tuple) const
-    {
-        auto hash1 = std::hash<T1> {} (std::get<0>(tuple));
-        auto hash2 = std::hash<T2> {} (std::get<1>(tuple));
-        return hash 1 ^ hash2;
-    }
-};
+
 class CourseGradebook : public Gradebook 
 {
+
 protected:
-    std::unordered_map<std::tuple<int, std::string>, double, tuple_hash> gradebook;
+    std::unordered_map<std::string, std::unordered_map<int, double>> scoresByAssignment;
+    std::unordered_map<int, std::unordered_map<std::string, double>> scoresByStudent;
+
 
 public:
     virtual ~CourseGradebook() 
@@ -28,42 +22,65 @@ public:
 
     std::unordered_map<int, double> GetAssignmentScores(std::string assignmentName) override 
     {
-       // Your code here (remove placeholder line below)
-        return std::unordered_map<int, double>();
+        auto it = scoresByAssignment.find(assignmentName);
+        if (it == scoresByAssignment.end())
+            return std::unordered_map<int, double>();
+
+        return it->second;
     }
 
     double GetScore(std::string assignmentName, int studentID) override 
     {
-       // Your code here (remove placeholder line below)
-        return NAN;
+        auto ait = scoresByAssignment.find(assignmentName);
+        if (ait == scoresByAssignment.end())
+            return NAN;
+        auto sit = ait->second.find(studentID);
+        if (sit == ait->second.end())
+            return NAN;
+
+        return sit->second;
     }
 
     std::vector<std::string> GetSortedAssignmentNames() override 
     {
-       // Your code here (remove placeholder line below)
-        return std::vector<std::string>();
+        std::vector<std::string> names;
+        names.reserve(scoresByAssignment.size());
+        for (auto &p : scoresByAssignment)
+            names.push_back(p.first);
+
+        std::sort(names.begin(), names.end());
+        return names;
     }
 
     // GetSortedStudentIDs() returns a vector with all distinct student IDs,
     // sorted in ascending order.
     std::vector<int> GetSortedStudentIDs() override 
     {
-       // Your code here (remove placeholder line below)
-        return std::vector<int>();
+        std::vector<int> ids;
+        ids.reserve(scoresByStudent.size());
+        for (auto &p : scoresByStudent)
+            ids.push_back(p.first);
+
+        std::sort(ids.begin(), ids.end());
+        return ids;
     }
 
     // GetStudentScores() gets all scores that exist in the gradebook for the
     // student whose ID equals the studentID parameter. Scores are returned as
     // an unordered_map that maps an assignment name to the student's
     // corresponding score for that assignment.
-    std::unordered_map<std::string, double> GetStudentScores(
-       int studentID) override {
-       // Your code here (remove placeholder line below)
-        return std::unordered_map<std::string, double>();
+    std::unordered_map<std::string, double> GetStudentScores(int studentID) override 
+    {
+        auto it = scoresByStudent.find(studentID);
+        if (it == scoresByStudent.end())
+            return std::unordered_map<std::string, double>();
+
+        return it->second;
     }
 
     void SetScore(std::string assignmentName, int studentID, double score) override {
-       // Your code here
+        scoresByAssignment[assignmentName][studentID] = score;
+        scoresByStudent[studentID][assignmentName] = score;
     }
 };
 
