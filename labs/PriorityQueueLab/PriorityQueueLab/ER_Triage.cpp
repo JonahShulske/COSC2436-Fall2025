827415fbb3206be6a87182eeddca50053ca35863
Jonah Shulske <jonah.shulske@my.tccd.edu>
2025-11-13 09:37:04 -0600

    It works it's good enough

--- labs/PriorityQueueLab/PriorityQueueLab/ER_Triage.cpp
+++ labs/PriorityQueueLab/PriorityQueueLab/ER_Triage.cpp
@@ -22,19 +22,39 @@ class TriageQueue
 {
 private:
     std::priority_queue<Patient> heap;
+    ostream* logStream = nullptr;
+    void Print(const std::string& message) const 
+    {
+        std::cout << message << endl;
+        if (logStream)
+            (*logStream) << message << endl;
+    }
 
 public:
     void Enqueue(const Patient& patient) 
     {
-        // insert the patient into the queue
         heap.push(patient);
-        
-        // Call visualize to display and log the entire queue
+        Visualize();
+    }
+
+    void SetLogStream(ostream* stream)
+    {
+        logStream = stream;
     }
 
     Patient Dequeue() 
     {
         // if the queue is empty throw an exception
+        if (heap.empty())
+            throw out_of_range("Error: The Triage is Empty");
+
+        Patient treated = heap.top();
+        heap.pop();
+
+        std::cout << "Treated Patient: " << treated.name << ", Priority: " << treated.priority << "\n" << endl;
+
+        Visualize();
+        return treated;
         
         // Display and log the patient treated and their priority
         
@@ -46,6 +66,9 @@ public:
     Patient Peek() const 
     {
         // Return the patient at the root or throw exception if the heap is empty
+        if (heap.empty())
+            throw out_of_range("Error: The Triage is Empty");
+        return heap.top();
     }
 	
 	bool IsEmpty() const 
@@ -55,6 +78,15 @@ public:
 
     void Visualize() const 
     {
+        priority_queue<Patient> tempHeap = heap;
+        std::ostringstream oss;
+        oss << "Current Triage Queue: ";
+        while (!tempHeap.empty()) 
+        {
+            oss << tempHeap.top().name << " (" << tempHeap.top().priority << ") ";
+            tempHeap.pop();
+        }
+        Print(oss.str());
         // Print the contents of the current priority queue
         // to both the display and the log file
     }
@@ -76,14 +108,24 @@ public:
 
 int main() 
 {
+    ofstream logFile("triage_log.txt");
+    if (!logFile.is_open())
+    {
+        cerr << "Error: File could not be opened" << endl;
+        return 1;
+    }
+
     TriageQueue triage;
+    triage.SetLogStream(&logFile);
     triage.LoadFromFile("patients.txt");
     
 	while (!triage.IsEmpty()) 
     {
-		cout << triage.Peek().name << endl;
+		std::cout << "Next Patient: " << triage.Peek().name << endl;
+        logFile << "Next Patient: " << triage.Peek().name << endl;
 		triage.Dequeue();
-	}	
-	
+	}
+
+    logFile.close();
     return 0;
 }
